c12bf0bb51aa5418b49f2dbdfa6cd5efdf62efdb
Matt Shanahan <mrshanahan11235@gmail.com>
2025-03-25 13:23:55 -0500

    Add title filter to index.html

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 // depends on notes.js
 
+var notesList;
+
 function loadNotes() {
     const token = validateAuthToken();
     if (!token) {
@@ -7,25 +9,41 @@ function loadNotes() {
     }
 
     getNotes(token, function (response) {
-        const notesList = document.getElementById('notes-list');
-        for (var i = 0; i < response.length; i++) {
-            var note = response[i];
-            const noteNode = document.createElement('div');
-            noteNode.className = 'note';
-            noteNode.setAttribute('onclick', "location.href = '/edit.html?id=" + note.id + "';");
-            noteNode.setAttribute('id', 'note-' + note.id);
-            noteNode.setAttribute('tabindex', '0');
-            const noteNodeTitle = document.createElement('div');
-            noteNodeTitle.className = 'note-title';
-            noteNodeTitle.innerText = note.title;
-            const noteNodePreview = document.createElement('div');
-            noteNodePreview.className = 'note-preview';
-            noteNodePreview.innerHTML = genericTextToHtmlText(note.content_preview);
-            noteNode.appendChild(noteNodeTitle);
-            noteNode.appendChild(noteNodePreview);
-            notesList.appendChild(noteNode);
-        }
+        notesList = response;
+        renderNotes();
     })
+}
+
+function renderNotes() {
+    const filter = document.getElementById('notes-list-filter').value;
+    const notes = notesList.filter(n => n.title.toLowerCase().indexOf(filter) >= 0);
+    const notesListElement = document.getElementById('notes-list');
+
+    // Not necessarily the fastest method to remove all children but good enough for us.
+    // See some excellent analysis here: https://stackoverflow.com/a/75900408/873216
+    notesListElement.replaceChildren();
+
+    for (var i = 0; i < notes.length; i++) {
+        const noteElement = createNoteElement(notes[i]);
+        notesListElement.appendChild(noteElement);
+    }
+}
+
+function createNoteElement(note) {
+    const noteNode = document.createElement('div');
+    noteNode.className = 'note';
+    noteNode.setAttribute('onclick', "location.href = '/edit.html?id=" + note.id + "';");
+    noteNode.setAttribute('id', 'note-' + note.id);
+    noteNode.setAttribute('tabindex', '0');
+    const noteNodeTitle = document.createElement('div');
+    noteNodeTitle.className = 'note-title';
+    noteNodeTitle.innerText = note.title;
+    const noteNodePreview = document.createElement('div');
+    noteNodePreview.className = 'note-preview';
+    noteNodePreview.innerHTML = genericTextToHtmlText(note.content_preview);
+    noteNode.appendChild(noteNodeTitle);
+    noteNode.appendChild(noteNodePreview);
+    return noteNode;
 }
 
 window.onload = loadNotes;
