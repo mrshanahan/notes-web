54a6ad7d5d902f2591d924ce700ec11790c67fab
Matt Shanahan <mrshanahan11235@gmail.com>
2025-04-11 23:01:58 -0500

    Use sync.Map for file cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -25,7 +26,7 @@ type fileCacheEntry struct {
 
 type fileCache struct {
 	Config FileCacheConfig
-	Cache  map[string]*fileCacheEntry
+	Cache  *sync.Map
 }
 
 func NewFileCache(config ...FileCacheConfig) Cache {
@@ -40,15 +41,15 @@ func NewFileCache(config ...FileCacheConfig) Cache {
 	}
 	return &fileCache{
 		Config: c,
-		Cache:  map[string]*fileCacheEntry{},
+		Cache:  &sync.Map{},
 	}
 }
 
 func (c *fileCache) Get(key string) ([]byte, error) {
 	path := filepath.Join(c.Config.RootDir, key)
-	entry, exists := c.Cache[key]
+	entry, exists := c.Cache.Load(key)
 	now := time.Now().UTC()
-	if !exists || entry.LoadedAt.Add(c.Config.ValidityInterval).Before(now) {
+	if !exists || entry.(*fileCacheEntry).LoadedAt.Add(c.Config.ValidityInterval).Before(now) {
 		file, err := os.Open(path)
 		if err != nil {
 			return nil, err
@@ -68,8 +69,8 @@ func (c *fileCache) Get(key string) ([]byte, error) {
 			ModTimeCheckedAt: now,
 			LastModTime:      modTime,
 		}
-		c.Cache[key] = entry
-	} else if entry.ModTimeCheckedAt.Add(c.Config.MetadataCheckInterval).Before(now) {
+		c.Cache.Store(key, entry)
+	} else if entry.(*fileCacheEntry).ModTimeCheckedAt.Add(c.Config.MetadataCheckInterval).Before(now) {
 		file, err := os.Open(path)
 		if err != nil {
 			return nil, err
@@ -79,7 +80,7 @@ func (c *fileCache) Get(key string) ([]byte, error) {
 			return nil, err
 		}
 		modTime := fileMetadata.ModTime().UTC()
-		if modTime.After(entry.LastModTime) {
+		if modTime.After(entry.(*fileCacheEntry).LastModTime) {
 			content, err := os.ReadFile(path)
 			if err != nil {
 				return nil, err
@@ -90,8 +91,8 @@ func (c *fileCache) Get(key string) ([]byte, error) {
 				ModTimeCheckedAt: now,
 				LastModTime:      modTime,
 			}
-			c.Cache[key] = entry
+			c.Cache.Store(key, entry)
 		}
 	}
-	return entry.Content, nil
+	return entry.(*fileCacheEntry).Content, nil
 }
